7ffa4254b7341f288845a398f5af4374d6bf5498
horsefacts <webmaster@terminally.online>
2024-01-04 15:39:14 -0500

    fix: export useSignInMessage

--- packages/connect-kit/src/hooks/index.ts
+++ packages/connect-kit/src/hooks/index.ts
@@ -1,6 +1,7 @@
 export * from "./useAppClient";
 export * from "./useConnect";
 export * from "./useSignIn";
+export * from "./useSignInMessage";
 export * from "./useUserData";
 export * from "./useVerifySignInMessage";
 export * from "./useWatchStatus";

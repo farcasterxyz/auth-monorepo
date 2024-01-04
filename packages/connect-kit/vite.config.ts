d3b75a35f5efd0bd86cfccdab869bab6f7edb20e
horsefacts <webmaster@terminally.online>
2024-01-03 23:55:05 -0500

    fix: bundle types, publish v0.0.5

--- packages/connect-kit/vite.config.ts
+++ packages/connect-kit/vite.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin(), nodePolyfills()],
+  plugins: [react(), vanillaExtractPlugin(), nodePolyfills(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),

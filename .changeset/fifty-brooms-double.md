844a5ff81e57d252bacf8682cbb73472ab25417c
Tony D'Addeo <tony@merklemanufactory.com>
2024-01-05 15:25:12 -0600

    feat: ship a polyfill for Buffer in connect-kit
    apg-js depends on the node Buffer implementation. Ship the polyfill to
    make life easier for devs.

---
+++ .changeset/fifty-brooms-double.md
@@ -0,0 +1,5 @@
+---
+"@farcaster/connect-kit": patch
+---
+
+ship a polyfill for Buffer
--- packages/connect-kit/vite.config.ts
+++ packages/connect-kit/vite.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import dts from "vite-plugin-dts";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin(), dts({ rollupTypes: true })],
+  plugins: [react(), vanillaExtractPlugin(), dts({ rollupTypes: true }), nodePolyfills({ include: ["buffer"] })],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),

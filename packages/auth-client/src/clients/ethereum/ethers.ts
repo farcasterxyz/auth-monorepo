a98eb9e546fc6034b402f0948cc286d7b4ff80fa
horsefacts <webmaster@terminally.online>
2024-01-11 18:16:50 -0500

    fix: destructure ethers

--- packages/auth-client/src/clients/ethereum/ethers.ts
+++ packages/auth-client/src/clients/ethereum/ethers.ts
@@ -1,11 +1,13 @@
 import * as ethers from "ethers";
 
+const { providers } = ethers;
+
 export const JsonRpcProvider =
   // @ts-expect-error -- ethers v6 compatibility hack
-  ethers.providers ? ethers.providers.JsonRpcProvider : ethers.JsonRpcProvider;
+  providers ? providers.JsonRpcProvider : ethers.JsonRpcProvider;
 
 export const Provider =
   // @ts-expect-error -- ethers v6 compatibility hack
-  ethers.providers ? ethers.providers.Provider : ethers.Provider;
+  providers ? providers.Provider : ethers.Provider;
 
 export type Provider = typeof Provider;

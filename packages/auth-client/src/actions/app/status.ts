d36e0439f4017b77f7048ae739eb948b5b154fcc
jamesdawsonwd <jamesdawsonwd@gmail.com>
2024-04-18 15:29:41 +0100

    fix: make StatusApiResponse type discrimitive union

--- packages/auth-client/src/actions/app/status.ts
+++ packages/auth-client/src/actions/app/status.ts
@@ -9,20 +9,25 @@ export interface StatusArgs {
 
 export type StatusResponse = AsyncUnwrapped<HttpResponse<StatusAPIResponse>>;
 
-export interface StatusAPIResponse {
-  state: "pending" | "completed";
-  nonce: string;
-  url: string;
-  message?: string;
-  signature?: `0x${string}`;
-  fid?: number;
-  username?: string;
-  bio?: string;
-  displayName?: string;
-  pfpUrl?: string;
-  verifications?: Hex[];
-  custody?: Hex;
-}
+export type StatusAPIResponse =
+  | {
+      state: "pending";
+      nonce: string;
+    }
+  | {
+      state: "completed";
+      nonce: string;
+      url: string;
+      message: string;
+      signature: `0x${string}`;
+      fid: number;
+      username: string;
+      bio: string;
+      displayName: string;
+      pfpUrl: string;
+      verifications?: Hex[];
+      custody?: Hex;
+    };
 
 const path = "channel/status";
 

951a6925b9aa4d3ed2e1f48690f42620e9793c21
horsefacts <109845214+horsefacts@users.noreply.github.com>
2024-05-14 07:53:38 -0700

    feat: improve QR code legibility (#168)
    ## Motivation
    
    The default AuthKit QR code can be difficult to scan.
    
    ## Change Summary
    
    Remove rounded dots, bump up default QR code size:
    
    <img width="355" alt="Screenshot 2024-05-10 at 7 40 50 PM"
    src="https://github.com/farcasterxyz/auth-monorepo/assets/109845214/77ee5ed4-34c6-4d6b-ba35-69cdbfc4f129">
    <img width="365" alt="Screenshot 2024-05-10 at 7 41 11 PM"
    src="https://github.com/farcasterxyz/auth-monorepo/assets/109845214/b64b8465-f260-446f-84ce-0fc1cdb0570d">
    
    ## Merge Checklist
    
    _Choose all relevant options below by adding an `x` now or at any time
    before submitting for review_
    
    - [x] PR title adheres to the [conventional
    commits](https://www.conventionalcommits.org/en/v1.0.0/) standard
    - [ ] PR has a changeset
    - [x] PR has been tagged with a change label(s) (i.e. documentation,
    feature, bugfix, or chore)
    - [x] PR includes documentation if necessary
    - [x] All commits have been signed
    
    ---------
    
    Co-authored-by: Vladyslav Dalechyn <vladyslav@spilnota.xyz>

--- packages/auth-kit/src/components/FarcasterLogo.tsx
+++ packages/auth-kit/src/components/FarcasterLogo.tsx
@@ -12,6 +12,7 @@ export function FarcasterLogo({
       xmlns="http://www.w3.org/2000/svg"
       width={width}
       height={height}
+      viewBox="0 0 22 20"
       fill="none"
     >
       <title>Farcaster logo</title>

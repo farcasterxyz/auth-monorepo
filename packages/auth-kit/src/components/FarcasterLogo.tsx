779cff64be5dffa75135f1b9be420f75b7d2273d
horsefacts <109845214+horsefacts@users.noreply.github.com>
2024-08-14 11:05:45 -0700

    fix: remove duplicate viewBox prop (#187)
    ## Motivation
    
    Describe why this issue should be fixed and link to any relevant design
    docs, issues or other relevant items.
    
    ## Change Summary
    
    Describe the changes being made in 1-2 concise sentences.
    
    ## Merge Checklist
    
    _Choose all relevant options below by adding an `x` now or at any time
    before submitting for review_
    
    - [x] PR title adheres to the [conventional
    commits](https://www.conventionalcommits.org/en/v1.0.0/) standard
    - [ ] PR has a changeset
    - [x] PR has been tagged with a change label(s) (i.e. documentation,
    feature, bugfix, or chore)
    - [x] PR includes documentation if necessary
    - [x] All commits have been signed
    
    ## Additional Context
    
    If this is a relatively large or complex change, provide more details
    here that will help reviewers

--- packages/auth-kit/src/components/FarcasterLogo.tsx
+++ packages/auth-kit/src/components/FarcasterLogo.tsx
@@ -14,7 +14,6 @@ export function FarcasterLogo({
       height={height}
       viewBox="0 0 22 20"
       fill="none"
-      viewBox="0 0 22 20"
     >
       <title>Farcaster logo</title>
       <g fill={fillColor[fill]} clipPath="url(#a)">

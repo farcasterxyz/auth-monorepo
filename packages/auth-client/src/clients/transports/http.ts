079406b2fdf5430e4af566ac2346093db65694a0
horsefacts <webmaster@terminally.online>
2024-08-14 16:03:41 -0400

    fix: linter error

--- packages/auth-client/src/clients/transports/http.ts
+++ packages/auth-client/src/clients/transports/http.ts
@@ -25,7 +25,7 @@ const defaultPollOpts = {
   interval: 1000,
   timeout: 10000,
   successCode: 200,
-  onResponse: () => { },
+  onResponse: () => {},
 };
 
 export const get = async <ResponseDataType>(
